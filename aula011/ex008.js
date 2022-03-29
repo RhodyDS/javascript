993f4d6d9a44c0c78daac2d832d40b29424b4d2b
Rhodytesla <naruto@ufpi.edu.br>
2022-03-29 15:08:22 -0300

    curso html e css 1

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.3
+
+console.log(`a velocidade do seu carro ${vel}Km/h`)
+if(vel > 60){
+    console.log(`voce ultrapassou a velocidade permitida.MULTADO!!`)
+
+}
+console.log(`dirija sempre usando cinto de segurança!!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+
+if(pais == 'brasil'){
+    console.log('vocÊ é brasileiro!')
+
+}else{
+    console.log('você é estrangeiro')
+}
